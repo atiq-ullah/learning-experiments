9b1199948701dc29484615560ef958e632a987a9
atiq-ullah <sultan.ullah1@icloud.com>
2023-09-09 19:26:12 -0700

    Scraping rotten tomatoes "in theatres"

--- Go/web-scraper/web-scraper.go
+++ Go/web-scraper/web-scraper.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
@@ -17,12 +19,27 @@ func main() {
 		// e.Request.Visit(e.Attr("href"))  // Uncomment this if you want to follow links
 
 		// Print the link
-		fmt.Println(e.Attr("href"))
+		// fmt.Println(e.Attr("href"))
 	})
 
 	// On every HTML element which has <title> tag call this
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		fmt.Println("Page title: ", e.Text)
+	// c.OnHTML("title", func(e *colly.HTMLElement) {
+	// 	fmt.Println("Page title: ", e.Text)
+	// })
+
+	c.OnHTML("a[data-track]", func(e *colly.HTMLElement) {
+		// Additional search within this span
+		e.DOM.Find("span.p--small").Each(func(index int, item *goquery.Selection) {
+			// Do something with each "anotherSelector" within this span
+			fmt.Print(strings.TrimSpace(item.Text()))
+			fmt.Print(" | ")
+		})
+
+		// Additional search within this span
+		e.DOM.Find("span.smaller").Each(func(index int, item *goquery.Selection) {
+			// Do something with each "anotherSelector" within this span
+			fmt.Println(strings.TrimSpace(item.Text()))
+		})
 	})
 
 	// Log errors
@@ -31,7 +48,8 @@ func main() {
 	})
 
 	// Start the web scraping by visiting a website
-	err := c.Visit("http://google.com")
+	// err := c.Visit("http://google.com")
+	err := c.Visit("https://www.rottentomatoes.com/browse/movies_in_theaters/sort:popular")
 	if err != nil {
 		log.Fatal(err)
 	}
