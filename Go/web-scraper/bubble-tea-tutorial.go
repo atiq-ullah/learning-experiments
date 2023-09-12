c810d364830b3ea30c5fa523b4aa1467fd2ef2db
atiq-ullah <sultan.ullah1@icloud.com>
2023-09-11 20:00:18 -0700

    More progress on tutorial

--- Go/web-scraper/bubble-tea-tutorial.go
+++ Go/web-scraper/bubble-tea-tutorial.go
@@ -1,5 +1,7 @@
 package main
 
+import tea "github.com/charmbracelet/bubbletea"
+
 type model struct {
 	choices  []string
 	cursor   int
@@ -7,5 +9,40 @@ type model struct {
 }
 
 func initalModel() model {
-	return model{}
+	return model{
+		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch message := msg.(type) {
+	case tea.KeyMsg:
+
+		switch message.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok = m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
 }
